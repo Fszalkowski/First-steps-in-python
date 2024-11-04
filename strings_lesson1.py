48bc99c9ddc6cfb0b77bdbe35a83eef277ad14ce
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-04 21:21:46 +0100

    Create strings_lesson1.py

---
+++ strings_lesson1.py
@@ -0,0 +1,7 @@
+homework = "Huge Polish kingdoms"
+
+print('Our thame is: Huge Polish Kingdoms')
+print((homework.upper())) #bigger_word_"lesson"
+print((homework.find('o'))) #in_with_letter_python_see_"o"
+print(homework.replace('o', '0')) #replaceing_"o"_to_"0"
+print('Polish' in homework)
