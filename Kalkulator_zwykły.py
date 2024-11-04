ee8b31bbe6578eb8bfa33491272502714fcd897c
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-04 20:30:54 +0100

    Update and rename Kalkulator_zwykły.py to simple_calculator.py

--- Kalkulator_zwykły.py
+++
@@ -1,4 +0,0 @@
-pierwsza = input("Podaj pierwszą liczbę: ")
-druga = input("Podaj drugą liczbę: ")
-sum = float(pierwsza) +float(druga)
-print("Wynik: " + str(sum))
---
+++ simple_calculator.py
@@ -0,0 +1,4 @@
+first = input("Enter first count: ") #entering_first_count
+second = input("Enter second count: ") #entering_second_count 
+sum = float(first) +float(second) 
+print("Result: " + str(sum)) #summary
