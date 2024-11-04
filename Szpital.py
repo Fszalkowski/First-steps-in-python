e62989042075ae539fb21bb53c02662bb3e33c6e
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-04 20:30:06 +0100

    Update and rename Szpital.py to hospital.py

--- Szpital.py
+++
@@ -1,6 +0,0 @@
-name = input("Jak masz na imię?")
-last_name = input("Jak się nazywasz?") #nowy pacjent
-age = input("Ile msasz lat? ")
-
-
-print("Dodano nowego pacjenta!", "Imie: " + name, "Nazwisko: " + last_name, "Wiek: " + age)
---
+++ hospital.py
@@ -0,0 +1,5 @@
+name = input("What's your name?") #question_about_name
+last_name = input("What's your surname?") #question_about_surname
+age = input("How many years do you have? ") #question_about_age
+
+print("Added new patient!", "Name: " + name, "Surname: " + last_name, "Age: " + age) #summary
