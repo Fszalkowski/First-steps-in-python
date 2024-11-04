5329294911bfb7f98ac124c836dccb5eda027f59
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-04 20:29:38 +0100

    Update and rename Kalkulator_wieku.py to age_calculator.py

--- Kalkulator_wieku.py
+++
@@ -1,3 +0,0 @@
-data_urodzenia = input("W którym roku się urodziłeś?" )
-wiek = 2024 - int(data_urodzenia)
-print("A więc masz:", wiek, "lat")
---
+++ age_calculator.py
@@ -0,0 +1,3 @@
+born_date = input("In with year do you born?" ) #born_date
+age = 2024 - int(born_date)
+print("And now you have:", age, "years") #summary
