50fa617d18c2092f1eb7bcf473e30714c20ab595
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-04 22:13:12 +0100

    Update logical_operathors.py
    Problem resolved :D

--- logical_operathors.py
+++ logical_operathors.py
@@ -1,3 +1,3 @@
 print("Let's check our price properties!")
 price = input('Enter your price item: ') #entering_price_item
-print('Your price was equal to 10?',  price == 10) #ERORR 10=/10 why?
+print('Your price was equal to 10?',  int(price) == 10) #comparing_price
