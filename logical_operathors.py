2dfeafd2212e936cfa6a65ac519b9c46d5a18755
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-05 12:58:07 +0100

    Update logical_operathors.py

--- logical_operathors.py
+++ logical_operathors.py
@@ -1,3 +1,6 @@
-print("Let's check our price properties!")
-price = input('Enter your price item: ') #entering_price_item
-print('Your price was equal to 10?',  int(price) == 10) #comparing_price
+print("Let's check our number properties!")
+
+number = input('Enter your number: ') #entering_number
+print('Or your number is equal to 18? ', int(number) == 18) #equaling
+print('Or your number is less than 25?', int(number) < 25) #less_than_our 
+print('Or your number is more than 60?', int(number) > 60) #more_than_our
