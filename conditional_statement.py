23e555e5dbf0d49800c0e3d716600838b51f88e7
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-06 12:01:56 +0100

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,14 @@
+math_grade = input('Enter your math grade: ') #entering_grade
+if int(math_grade) > 3: 
+    print("Nice!")
+elif int(math_grade) < 3:
+    print("Let's correct it!") #conparison
+    
+pe_grade = input('Enter your pe grade: ') #entering_grade
+if int(pe_grade) > 4:
+    print("Nice!")
+elif int(pe_grade) ==4: #conparison
+    print("Not bad :/")
+elif int(pe_grade) < 4:
+    print("You are looser?")
+    
