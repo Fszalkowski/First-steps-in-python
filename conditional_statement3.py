5758102695a76c7990c1c3ef8920fd4fab66f489
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-07 11:41:40 +0100

    Create conditional_statement3.py

---
+++ conditional_statement3.py
@@ -0,0 +1,8 @@
+count = input("Enter random count: ")
+count_2 = input("Enter second random count: ")
+
+if int(count) == int(count_2):
+    print("Your second count was equals first count")
+else:
+    print("Your second count wasn't equals frist count")
+#in_progress
