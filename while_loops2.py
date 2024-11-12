ba25855008355cff7f14ab7a61f07ba386e0b9ff
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-12 20:43:38 +0100

    Create while_loops2.py

---
+++ while_loops2.py
@@ -0,0 +1,8 @@
+count = int(input("Enter random count: "))
+print("Now we can start loop training!")
+
+while count < 10:
+    print(count)
+    count = count + 1
+    
+print("Trening was end")    
