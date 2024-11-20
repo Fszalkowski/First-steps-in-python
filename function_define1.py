a4579441a2be063789f39141ff8605a4936db1c7
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-20 21:05:53 +0100

    Add files via upload

---
+++ function_define1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+def welcome(first_name, last_name):
+    print(f"Welcome {first_name} {last_name}")
+
+welcome(name, surname)    
