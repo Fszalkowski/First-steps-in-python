dff4bc2060d577cd7106b3c137fbef200401ca7d
Filip Szałkowski <filip_szalkowski04@wp.pl>
2024-11-27 09:53:32 +0100

    Update list_for1.py

--- list_for1.py
+++ list_for1.py
@@ -3,11 +3,22 @@ while True:
         fruit_1 = input("Enter first fruit: ")
         fruit_2 = input("Enter second fruit: ")
         fruit_3 = input("Enter third fruit: ")
+    
+        fruits = [fruit_1, fruit_2, fruit_3]
+        if "banana" in fruits:
+            for n, fruit in enumerate(fruits):
+                if fruit == "banana":
+                    print(f"Banana was very sweet!")
+                    
+        exit_button = input("You wanna exit?(yes/no)")
+        if exit_button == "yes":
+            break
+        elif exit_button == "no":
+            continue
+        else:
+            print("Invalid value")
+            break
     except ValueError:
         print("Invalid value")
         continue
-    fruits = [fruit_1, fruit_2, fruit_3]
-    for fruit in fruits:
-        print(fruit)
-    break
     #in_progress
